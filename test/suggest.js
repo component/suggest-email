b990e21896ff0c905718ecc3582616aa2eda30a0
TJ Holowaychuk <tj@vision-media.ca>
2012-05-08 11:42:50 -0700

    test for suggest.domains

--- test/suggest.js
+++ test/suggest.js
@@ -1,6 +1,12 @@
 
 var suggest = require('../');
 
+describe('suggest.domains', function(){
+  it('should be exposed', function(){
+    suggest.domains.should.be.an.instanceof(Array);
+  })
+})
+
 describe('suggest(email)', function(){
   describe('when @ is not yet present', function(){
     it('should return ""', function(){
