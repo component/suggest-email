b5b14a7e3e2df315042ec40e8b8b189f4e496d52
TJ Holowaychuk <tj@vision-media.ca>
2012-09-15 19:03:37 -0700

    add component.json

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 
-module.exports = require('./lib/suggest-email');
+/**
+ * Expose `suggest()`.
+ */
+
+exports = module.exports = suggest;
+
+/**
+ * Suggest an email domain based on the given `str`
+ * or "" when no match is found.
+ *
+ * @param {String} str
+ * @return {String}
+ * @api public
+ */
+
+function suggest(str){
+  str = str.split('@')[1];
+  if (!str) return '';
+
+  var match = exports.domains.filter(function(domain){
+    return 0 == domain.indexOf(str);
+  }).shift() || '';
+
+  return match.replace(str, '');
+}
+
+/**
+ * Domains.
+ */
+
+exports.domains = [
+  'yahoo.com',
+  'gmail.com',
+  'google.com',
+  'hotmail.com',
+  'me.com',
+  'aol.com',
+  'mac.com',
+  'live.com',
+  'comcast.com',
+  'googlemail.com',
+  'msn.com',
+  'hotmail.co.uk',
+  'yahoo.co.uk',
+  'facebook.com',
+  'verizon.net',
+  'att.net',
+  'gmz.com',
+  'mail.com',
+];
